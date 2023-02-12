b9370255c9e37c4bd775feacec7c33c80a6b3583
mayur-ud <85556600+mayur-ud@users.noreply.github.com>
2023-02-12 23:24:06 +0530

    Update socket.js
    updated env variables

--- src/socket.js
+++ src/socket.js
@@ -7,5 +7,5 @@ export const initSocket = async () => {
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io('http://localhost:5000' , options);    // return io(process.env.REACT_APP_BACKEND_URL, options);
+    return io(process.env.REACT_APP_FRONTEND_URL , options);    // return io(process.env.REACT_APP_BACKEND_URL, options);
 };
